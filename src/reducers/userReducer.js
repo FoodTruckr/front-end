ad23fc4d998443fe59a9efeb717af131f22e727a
AD9018 <adamdadi23@gmail.com>
2021-03-29 19:45:34 -0400

    action-reducer-33% complete

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,14 +1,71 @@
-// import{ }
+import {
+  SIGNUP_LOADING,
+  SIGNUP_SUCCESS,
+  SIGNUP_FAILURE,
+  LOGIN_LOADING,
+  LOGIN_SUCCESS,
+  LOGIN_FAILURE,
+  USER_LOGOUT,
+} from "../actions/userAction";
 
 const initialState = {
-  user: JSON.parse(window.localStorage.getItem("userInfo")) || {},
+  userId: "",
+  username: "",
+  role: "",
+  email: "",
   isLoading: false,
-  isLoggedin: window.localStorage.getItem("token"),
+  token: window.localStorage.getItem("token") || false,
   error: "",
 };
 
 const userReducer = (state = initialState, action) => {
   switch (action.type) {
+    case SIGNUP_LOADING:
+      return {
+        ...state,
+        isLoading: true,
+        error: "",
+      };
+    case SIGNUP_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        username: action.payload.username,
+      };
+    case SIGNUP_FAILURE:
+      return {
+        ...state,
+        isLoading: false,
+        error: action.payload.error,
+      };
+    case LOGIN_LOADING:
+      return {
+        ...state,
+        isLoading: true,
+        error: "",
+      };
+    case LOGIN_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        userId: action.payload.userId,
+        username: action.payload.username,
+        role: action.payload.role,
+        email: action.payload.email,
+        token: action.payload.token,
+      };
+    case LOGIN_FAILURE:
+      return {
+        ...state,
+        isLoading: false,
+        error: action.payload.error,
+      };
+    case USER_LOGOUT:
+      return {
+        ...state,
+        token: window.localStorage.removeItem("token") || false,
+      };
+
     default:
       return state;
   }
