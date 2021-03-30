a40c162faf0091dd1f4d37d976485d436250bdcd
CarlosSantiago Nevett <csnevett@gmail.com>
2021-03-30 11:24:15 -0400

    truckDinerReducer initState

--- src/reducers/truckDinerReducer.js
+++ src/reducers/truckDinerReducer.js
@@ -1,6 +1,77 @@
-// import{ }
+import {
+  GET_DINER_SUCCESS,
+  GET_DINER_FAILURE,
+  GET_DINER_LOADING,
+  GET_DINER_SINGLE_TRUCK_SUCCESS,
+  GET_DINER_SINGLE_TRUCK_FAILURE,
+  GET_DINER_SINGLE_TRUCK_LOADING,
+  GET_DINER_ALL_TRUCK_SUCCESS,
+  GET_DINER_ALL_TRUCK_FAILURE,
+  GET_DINER_ALL_TRUCK_LOADING,
+  POST_FAVORITE_SUCCESS,
+  POST_FAVORITE_FAILURE,
+  POST_FAVORITE_LOADING,
+  DELETE_FAVORITE_SUCCESS,
+  DELETE_FAVORITE_FAILURE,
+  DELETE_FAVORITE_LOADING,
+  POST_RATING_SUCCESS,
+  POST_RATING_FAILURE,
+  POST_RATING_LOADING,
+  PUT_RATING_LOADING,
+  PUT_RATING_SUCCESS,
+  PUT_RATING_FAILURE,
+  DELETE_RATING_LOADING,
+  DELETE_RATING_SUCCESS,
+  DELETE_RATING_FAILURE,
+  GET_CUISINE_TYPE_LOADING,
+  GET_CUISINE_TYPE_SUCCESS,
+  GET_CUISINE_TYPE_FAILURE,
+} from '../actions/truckDinerAction';
 
-const initialState = {};
+const initialState = {
+  diner: {
+    userId: '',
+    username: '',
+    favoriteTrucks: [
+      {
+        truckId: '',
+        truckName: '',
+      },
+    ],
+  },
+
+  singleTruck: {
+    truckId: '',
+    truckName: '',
+    currentLocation: '',
+    imageOfTruck: '',
+    cuisineTypes: [],
+    customerRatingAvg: '',
+    customerRatings: [{ username: '', starRatings: '' }],
+    arrivalTime: '',
+    arrivalDate: '',
+    departureTime: '',
+    departureDate: '',
+    menu: [
+      {
+        itemName: '',
+        itemDescription: '',
+        itemPrice: '',
+        itemPhotoUrl: '',
+      },
+    ],
+  },
+
+  trucks: [
+    {
+      truckId: '',
+      truckName: '',
+      imageOfTruck: '',
+      customerRatingAvg: '',
+      cuisineTypes: [],
+    },
+  ],
+};
 
 const truckDinerReducer = (state = initialState, action) => {
   switch (action.type) {
