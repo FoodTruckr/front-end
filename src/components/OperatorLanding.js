9110ea253d3bfd8068f024f3b8d243dc7a4281e8
AD9018 <adamdadi23@gmail.com>
2021-03-31 15:22:23 -0400

    FinishedSkeletonForOperatorLanding and Changed Trucklist component name to DinerLanding

--- src/components/OperatorLanding.js
+++ src/components/OperatorLanding.js
@@ -2,20 +2,33 @@ import { Panel } from "react95";
 import { Hourglass } from "react95";
 import { connect } from "react-redux";
 import { useEffect } from "react";
+import TruckListItem from "./TruckListItem";
+import { getOperator } from "../actions/truckOperatorAction";
 
-const OperatorLanding = () => {
-  const {} = props;
+const OperatorLanding = (props) => {
+  const { getOperator } = props;
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    getOperator();
+  }, []);
 
-  return <Panel>{}</Panel>;
+  return (
+    <Panel>
+      {props.isLoading && <h3>{Hourglass}</h3>}
+      {props.error && <p>{props.error}</p>}
+      {props.operator.trucksOwned.map((truck) => (
+        <TruckListItem key={truck.truckId} truck={truck} />
+      ))}
+    </Panel>
+  );
 };
 
 const mapStateToProps = (state) => {
   return {
-    isLoading: state.isLoading,
-    error: state.error,
+    isLoading: state.truckOperator.isLoading,
+    error: state.truckOperator.error,
+    operator: state.truckOperator.operator,
   };
 };
 
-export default connect(mapStateToProps, {})(OperatorLanding);
+export default connect(mapStateToProps, { getOperator })(OperatorLanding);
